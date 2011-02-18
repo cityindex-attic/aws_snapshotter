9d4b0f769ab7ec40d1f8d86f7c70935badbe7df5
Ryan Geyer <me@ryangeyer.com>
2011-02-17 17:13:18 -0800

    Been too long since I committed.. Added SNS notifications and snapshot pruning

--- create_snaps.rb
+++ create_snaps.rb
@@ -3,15 +3,56 @@ require 'right_aws'
 require 'settings'
 
 transition_change_timeouts  = [5, 10, 30, 60, 90, 120] # Total of 315 seconds, or just over 5 minutes
+timeout_sum                 = 0
 all_stopped                 = false
 all_started                 = false
 volumes_snapshotted         = 0
 startTs                     = Time.now
 startDate                   = startTs.strftime("%Y%m%d%H%M%S")
+$messages                   = []
 
-Settings.get_credentials()
+def log(message)
+  puts message
+  $messages << message
+end
+
+def format_messages_for_sns
+  retVal = ''
+  $messages.each {|message| retVal += message + "
+"}
+  retVal
+end
+
+transition_change_timeouts.each{|t| timeout_sum += t}
+
+Settings.get_settings()
 
 ec2 = Rightscale::Ec2.new(Settings.aws_access_key_id, Settings.aws_secret_access_key)
+sns = Rightscale::SnsInterface.new(Settings.aws_access_key_id, Settings.aws_secret_access_key)
+sns_configured = false
+
+if Settings.notify_by_sns
+  if Settings.sns_topic_id.right_blank? && Settings.create_sns
+    topic_arn = sns.create_topic('AWSSnapshoterEvents')
+    if topic_arn
+      lines = File.readlines('./aws_snapshotter_settings.rb')
+      File.open('./aws_snapshotter_settings.rb', 'w') do |f|
+        lines.each do |line|
+          line.gsub!(/Settings::sns_topic_id(\s*)=.*$/,'Settings::sns_topic_id\1=' +" '#{topic_arn}'")
+          f.puts(line)
+        end
+      end
+
+      Settings.sns_subscribers.each do |subscriber|
+        sns.subscribe(topic_arn, 'email', subscriber)
+      end
+
+      require './aws_snapshotter_settings.rb'
+    end
+  end
+
+  sns_configured = sns.list_topics().collect{|topic| topic[:arn]}.include?(Settings.sns_topic_id)
+end
 
 instances = ec2.describe_instances()
 
@@ -28,8 +69,8 @@ instances_to_snapshot.delete_if { |instance| !Settings.instances.include?(instan
 ids = instances_to_snapshot.collect { |i| i[:aws_instance_id] }
 
 if ids != []
-  puts "About to stop the following AWS EC2 instances..."
-  ids.each { |id| puts id }
+  log "About to stop the following AWS EC2 instances..."
+  ids.each { |id| log "AWS Instance ID: #{id}" }
 
   ec2.stop_instances(ids)
 
@@ -39,7 +80,7 @@ if ids != []
     if all_stopped
       break
     else
-      puts "Waiting (#{timeout})s for instances to stop..."
+      log "Waiting (#{timeout})s for instances to stop..."
       sleep(timeout)
     end
   end
@@ -47,16 +88,32 @@ if ids != []
   if !all_stopped
     # TODO: What do we want to do here?  After all is said and done, we really need for the servers to be started again
     # even if a snapshot doesn't get taken.
-    puts "Even after all the waiting, not all instances were stopped, this is critical.. I should notify someone..."
+    log "After waiting #{timeout_sum} for #{instances_to_snapshot.count} instances to stop, some instances remain running"
+    sns.publish(Settings.sns_topic_id, format_messages_for_sns,
+                "Instance Stop Failed!") if sns_configured
   else
-    puts "All instances are stopped, proceeding with snapshots..."
+    log "All instances are stopped, proceeding with snapshots..."
     instances_to_snapshot.each do |instance|
       instance[:block_device_mappings].each do |mapping|
-        puts "Creating snapshot of volume #{mapping[:ebs_volume_id]} from instance #{instance[:aws_instance_id]}"
+        log "Creating snapshot of volume #{mapping[:ebs_volume_id]} from instance #{instance[:aws_instance_id]}"
         volumes_snapshotted += 1
         ec2.create_snapshot(mapping[:ebs_volume_id], "aws_snapshotter automated snapshot of #{mapping[:ebs_volume_id]} at #{startDate}")
 
-        # TODO: Prune old snapshots
+        snapshots = []
+        ec2.describe_snapshots.sort {|a,b| b[:aws_started_at] <=> a[:aws_started_at]}.each do |snapshot|
+          if snapshot[:aws_volume_id] == mapping[:ebs_volume_id]
+            snapshots << snapshot
+          end
+        end
+
+        if snapshots.count > Settings.snapshots_to_keep
+          num_to_delete = snapshots.count - Settings.snapshots_to_keep
+          snapshots.each_with_index do |snapshot,idx|
+            if idx == (num_to_delete-1) then break end
+            log "Deleting AWS Snapshot ID: #{snapshot[:aws_id]}"
+            ec2.delete_snapshot(snapshot[:aws_id])
+          end
+        end
       end
     end
   end
@@ -69,17 +126,23 @@ if ids != []
     if all_started
       break
     else
-      puts "Waiting (#{timeout})s for instances to start..."
+      log "Waiting (#{timeout})s for instances to start..."
       sleep(timeout)
     end
   end
 
   if !all_started
-    puts "Even after all the waiting, not all instances were started, this is critical.. I should notify someone..."
+    log "After waiting #{timeout_sum} to restart #{instances_to_snapshot.count} instances, some were still not running!"
+    sns.publish(Settings.sns_topic_id,
+                format_messages_for_sns,
+                "Instance Restart Failed!") if sns_configured
   else
-    puts "Finished creating #{volumes_snapshotted} snapshots from #{ids.count} instances in #{Time.now.to_i - startTs.to_i} seconds.  You've been snapshotted!"
+    log "Finished creating #{volumes_snapshotted} snapshots from #{ids.count} instances in #{Time.now.to_i - startTs.to_i} seconds.  You've been snapshotted!"
+
+    sns.publish(Settings.sns_topic_id, format_messages_for_sns, "Snapshots Completed!")
   end
 
 else
-  puts "No running instances were found to create snapshots of.  Double check your settings."
+  log "No running instances were found to create snapshots of.  Double check your settings."
+  sns.publish(Settings.sns_topic_id, format_messages_for_sns, "AWS Snapshotter Invalid Configuration")
 end
--- settings.rb
+++ settings.rb
@@ -1,7 +1,12 @@
 class Settings
-  @@aws_access_key_id = nil
-  @@aws_secret_access_key = nil
-  @@instances = []
+  @@aws_access_key_id       = nil
+  @@aws_secret_access_key   = nil
+  @@instances               = []
+  @@snapshots_to_keep       = 7
+  @@notify_by_sns           = false
+  @@create_sns              = false
+  @@sns_topic_id            = nil
+  @@sns_subscribes          = []
 
   def self.aws_access_key_id
     @@aws_access_key_id
@@ -21,8 +26,38 @@ class Settings
   def self.instances=(newval)
     @@instances = newval
   end
+  def self.snapshots_to_keep
+    @@snapshots_to_keep
+  end
+  def self.snapshots_to_keep=(newval)
+    @@snapshots_to_keep = newval
+  end
+  def self.notify_by_sns
+    @@notify_by_sns
+  end
+  def self.notify_by_sns=(newval)
+    @@notify_by_sns = newval
+  end
+  def self.create_sns
+    @@create_sns
+  end
+  def self.create_sns=(newval)
+    @@create_sns = newval
+  end
+  def self.sns_topic_id
+    @@sns_topic_id
+  end
+  def self.sns_topic_id=(newval)
+    @@sns_topic_id = newval
+  end
+  def self.sns_subscribers
+    @@sns_subscribers
+  end
+  def self.sns_subscribers=(newval)
+    @@sns_subscribers = newval
+  end
 
-  def self.get_credentials
+  def self.get_settings
     begin
     require './aws_snapshotter_settings'
     rescue Exception => e
